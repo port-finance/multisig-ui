a634c083ff63c78f4c4575e28a0dc7bde7298ecf
Maxim Piessen <maximpiessen@Maxims-MacBook-Pro.local>
2022-01-25 14:23:49 +0100

    update rpc endpoint

--- src/context/connection.tsx
+++ src/context/connection.tsx
@@ -16,7 +16,7 @@ export type ENV =
 export const ENDPOINTS = [
   {
     name: "mainnet-beta" as ENV,
-    endpoint: "https://solana-api.projectserum.com/",
+    endpoint: "https://ssc-dao.genesysgo.net/",
     chainID: ChainID.MainnetBeta,
   },
   {
