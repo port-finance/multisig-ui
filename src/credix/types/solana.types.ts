ded28d6fbdd4a49c23a9355331eab243739f0b53
Maxim Piessen <maximpiessen@maxims-mbp.home>
2022-01-31 15:12:53 +0100

    change order of dropdown

--- src/credix/types/solana.types.ts
+++ src/credix/types/solana.types.ts
@@ -1,13 +1,13 @@
 export enum SolanaCluster {
+	MAINNET = "mainnet-beta",
 	LOCALNET = "localnet",
 	DEVNET = "devnet",
-	MAINNET = "mainnet-beta",
 }
 
 export enum RPCEndpoint {
+	MAINNET = "https://api.mainnet-beta.solana.com",
 	LOCALNET = "http://127.0.0.1:8899",
 	DEVNET = "https://api.devnet.solana.com",
-	MAINNET = "https://api.mainnet-beta.solana.com",
 }
 
 export type PdaSeeds = Array<Buffer | Uint8Array>;
